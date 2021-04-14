b442825420da5df9b574c72a0cc318fb00b2de0a
liyasthomas <liyascthomas@gmail.com>
2021-04-14 23:30:23 +0530

    feat: redirects

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
-    class="flex flex-col mt-8 space-y-4 text-sm md:space-x-3 md:flex-row md:space-y-0"
+    class="flex flex-col items-start mt-8 space-y-4 text-sm md:space-x-3 md:flex-row md:space-y-0"
   >
     <a href="https://twitter.com/liyasthomas" target="_blank">Twitter</a>
     <a href="https://www.linkedin.com/in/liyasthomas" target="_blank"
@@ -8,7 +8,7 @@
     >
     <a href="mailto:liyascthomas@gmail.com">E-mail</a>
     <a href="https://github.com/liyasthomas" target="_blank">GitHub</a>
-    <a href="http://calendly.com/liyasthomas/30m" target="_blank">Calendar</a>
+    <a href="https://calendly.com/liyasthomas/30m" target="_blank">Calendar</a>
   </footer>
 </template>
 
