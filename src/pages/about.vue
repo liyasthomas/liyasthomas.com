d534f83de04f07b2a8db0951a95132906a748d5a
liyasthomas <liyascthomas@gmail.com>
2021-05-05 08:07:22 +0530

    contact form ui

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,71 +1,88 @@
 <template>
   <h1 class="inline-flex mb-8 text-2xl font-bold">{{ t("button.about") }}</h1>
-  <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label
-        >Your Role:
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label
-      >
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
-
   <form>
-    <p>
-      <label>
-        Your Name: <input type="text" name="name" v-model="form.name" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Email: <input type="email" name="email" v-model="form.email" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Message: <textarea name="message" v-model="form.message" />
-      </label>
-    </p>
-    <p>
-      <button type="submit" @click.prevent="handleSubmit">Send</button>
-    </p>
+    <div class="flex flex-col space-y-2 max-w-sm">
+      <input
+        type="text"
+        name="name"
+        v-model="form.name"
+        class="form-input"
+        placeholder="Name"
+      />
+      <input
+        class="form-input"
+        type="email"
+        name="email"
+        v-model="form.email"
+        placeholder="E-mail"
+      />
+      <textarea
+        name="message"
+        v-model="form.message"
+        rows="10"
+        class="form-input"
+        placeholder="Message"
+      >
+      </textarea>
+      <div>
+        <button type="submit" class="form-button" @click.prevent="handleSubmit">
+          Send
+        </button>
+      </div>
+    </div>
   </form>
 </template>
+
+<style scoped>
+.form-input {
+  @apply flex;
+  @apply px-5;
+  @apply py-3;
+  @apply rounded-lg;
+  @apply text-gray-800;
+  @apply bg-gray-100;
+  @apply focus:(outline-none bg-gray-200);
+  @apply focus-within:(ring ring-pink-400);
+  @apply hover:bg-gray-200;
+  @apply dark:(text-gray-50 bg-gray-800 focus:bg-gray-700 focus-within:ring-indigo-600 hover:bg-gray-700);
+  @apply transition-all;
+}
+.form-button {
+  @apply px-5;
+  @apply py-3;
+  @apply rounded-lg;
+  @apply font-bold;
+  @apply text-gray-50;
+  @apply bg-indigo-600;
+  @apply focus:outline-none;
+  @apply focus-within:(ring ring-pink-400);
+  @apply hover:bg-indigo-800;
+  @apply dark:(text-gray-900 bg-pink-400 focus-within:ring-indigo-600 hover:bg-pink-200);
+  @apply transition-all;
+  @apply cursor-pointer;
+}
+</style>
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n"
 const { t } = useI18n()
 import { useHead } from "@vueuse/head"
+import { ref } from "vue"
 
 useHead({
   title: "About - Liyas Thomas",
 })
 
-const form = {
+const form = ref({
   name: "",
   email: "",
   message: "",
-}
+})
 
-const encode = (data) => {
-  return Object.keys(data)
+const encode = (data) =>
+  Object.keys(data)
     .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
     .join("&")
-}
 
 const handleSubmit = () => {
   fetch("/", {
@@ -73,7 +90,7 @@ const handleSubmit = () => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: encode({ "form-name": "contact", ...form }),
   })
-    .then(() => alert("Success!"))
+    .then(() => alert("Thank you! Your message has been successfully sent."))
     .catch((error) => alert(error))
 }
 </script>
