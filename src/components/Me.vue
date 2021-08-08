b108b18cebeb0021d2991312d7a022980fd16f4f
liyasthomas <liyascthomas@gmail.com>
2021-08-08 15:00:43 +0530

    chore(deps): bump

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -6,13 +6,6 @@
         Hi, my name is Liyas Thomas, I'm a designer, developer, and an
         entrepreneur.
       </p>
-      <p>At first I intended to be an artist and started creating art works.</p>
-      <p>
-        In high school, I was that kid that seemed to never be paying attention.
-        Seemingly in my own world of doodles and drawings. I've had a pencil in
-        my hand since I was in diapers. Things never change... I'm no longer in
-        diapers though.
-      </p>
     </div>
   </div>
 </template>
